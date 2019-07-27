c6b6857fc4711c80a41b0007ef5d78df1ba345f6
Juwar <juwarthunderboy@gmail.com>
2019-07-27 10:54:15 +0700

    update

--- src/Api/Fire.js
+++ src/Api/Fire.js
@@ -9,13 +9,13 @@ class Fire {
     init = () => {
         if (!firebase.apps.length) {
             firebase.initializeApp({
-                apiKey: "AIzaSyB3q17F8tBExoDw8mgf48rIuLadvfV7KLc",
-                authDomain: "simplechatwithmaps.firebaseapp.com",
-                databaseURL: "https://simplechatwithmaps.firebaseio.com",
-                projectId: "simplechatwithmaps",
+                apiKey: "YOUR_APIKEY",
+                authDomain: "YOUR_AUTH_DOMAIN",
+                databaseURL: "YOUR_DATABASE",
+                projectId: "YOUR_PROJECT",
                 storageBucket: "",
-                messagingSenderId: "926693418671",
-                appId: "1:926693418671:web:af9970ae6e7c1ff7"
+                messagingSenderId: "YOR_SENDER_ID",
+                appId: "APP_ID"
             });
         }
     };
